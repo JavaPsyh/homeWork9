0682e66bcee5f742f7f3355bf3e7fa05a0bc66ab
JavaPsyh <psyhjava@gmail.com>
2019-06-10 16:40:42 +0300

    for TA+

--- src/ersatz/linked/list/ErsatzList.java
+++ src/ersatz/linked/list/ErsatzList.java
@@ -3,13 +3,20 @@ package ersatz.linked.list;
 public interface ErsatzList<T> {
 
     void add(T value);
-    void add(T value, int index);
-    void addAll(ErsatzList<T> ersatzList);
-    T get(int index);
-    void set(T value, int index);
-    T remove(int index); // возвращаем элемент, который удалили
-    T remove(T t); // удалять первый, который встретится
-    int size();
-    boolean isEmpty();
 
+    void add(T value, int index);
+
+    void addAll(ErsatzList<T> ersatzList);
+
+    T get(int index);
+
+    void set(T value, int index);
+
+    T remove(int index); // возвращаем элемент, который удалили
+
+    T remove(T t); // удалять первый, который встретится
+
+    int size();
+
+    boolean isEmpty();
 }
